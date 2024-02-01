0504e35dcab9ee7626d2e747872e41c875549f02
Harsha-r7 <harsharr440@gmail.com>
2024-02-01 12:33:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import sys
 def add(a,b):
     return a+b
+def sub(a, b):
+    return a-b
 if __name__ == "__main__":
-    a ,b=sys.argv[1], sys.argv[2]
+    print("pycharm")
+    print(add(5,2))
+    print(sub(5,2))
 
-    
